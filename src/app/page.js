8f363ba8ee768a388e4eb421c265b47fc53d1edc
unknown <gazinafisrafi.gnr@gmail.com>
2025-01-26 07:27:34 +0600

    fix: contact us also created

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Categories from "@/components/Categories/Categories";
+import ContactUs from "@/components/ContactUs/ContactUs";
 import FeaturedRecipes from "@/components/FeaturedRecipes/FeaturedRecipes";
 import Hero from "@/components/Hero/Hero";
 import Reviews from "@/components/Reviews/Reviews";
@@ -11,6 +12,7 @@ export default function Home() {
       <Hero/>
       <Reviews/>
       <FeaturedRecipes/>
+      <ContactUs/>
     </div>
   );
 }
--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import Image from "next/image"
+import Button from "../Button/Button"
 
-const ContactUs = () => {
-    return (
-        <section>
-            
-        </section>
-    );
-};
+const HeroSection = () => {
+  return (
+    <section className="px-4 sm:px-6 md:px-20 my-8 sm:my-10 md:my-20">
+        <div className="bg-primary relative rounded-xl pt-10 md:pt-20 pb-10">
+        <h1 className="text-3xl sm:text-4xl md:text-6xl font-semibold text-center">
+        Deliciousness to your inbox
+      </h1>
+      <p className="text-sm sm:text-base md:text-lg mt-3 md:mt-6 font-thin text-center text-gray-500 w-[90%] sm:w-[70%] md:w-1/2 mx-auto">
+        We offer a wide variety of healthy and delicious meals that are perfect for any occasion. Our meals are made
+        with fresh, high-quality ingredients and are prepared by our team of experienced chefs.
+      </p>
+      <div className="mt-8 md:mt-16 bg-white p-2 sm:p-3 rounded-xl w-[90%] sm:w-[80%] md:w-[480px] mx-auto flex flex-col sm:flex-row gap-3 sm:gap-0 justify-between">
+        <input
+          className="ml-2 sm:ml-6 w-full sm:w-auto focus:outline-none"
+          type="text"
+          name="text"
+          placeholder="Your email address here"
+        />
+        <Button bg={"bg-black"} className={"text-white w-full sm:w-auto"}>
+          Subscribe
+        </Button>
+      </div>
+      <div className="hidden lg:block lg:w-[280px] xl:w-[410px] absolute left-0 bottom-0">
+        <Image alt="vegetable" className="w-full" src={"/assets/inbox-veg.png"} width={400} height={400} />
+      </div>
+      <div className="hidden lg:block w-[280px] xl:w-[410px] absolute right-0 bottom-0">
+        <Image alt="vegetable" className="w-full" src={"/assets/inbox-noodles.png"} width={400} height={400} />
+      </div>
+        </div>
+      
+    </section>
+  )
+}
 
-export default ContactUs;
+export default HeroSection
+
